1912f27c45fcfccfa4220ecc0c6a2c18a2601316
naknamu <jodeldelvalle@gmail.com>
2023-01-11 17:45:17 +0800

    Added useWindowWidth hook to get screen size for the image mapper library

--- src/components/main.js
+++ src/components/main.js
@@ -1,50 +1,18 @@
-import { useState } from "react";
 import styled from "styled-components";
-import extendedCity from "../assets/images/extended_city.jpg";
-import Locate from "./locate";
-import Mapper from "./mapper"
+import Mapper from "./mapper";
+import {useWindowWidth} from '@react-hook/window-size';
+  
 
 const Main = () => {
 
-    const [location , setLocation] = useState([]);
-
-    const getCoordinates = (event) => {
-
-        let e = event.target;
-        let dim = e.getBoundingClientRect();
-        var x = event.clientX - dim.left;
-        var y = event.clientY - dim.top;
-
-        return [x, y];
-    }
-    
-    const handleClick = (event) => {
-        //get click coordinates
-        let [x , y] = getCoordinates(event);
-
-        console.log([x , y]);
-
-        let newLocation = [...location];
-
-        newLocation.push(<Locate key={location.length + 1} posX={x} posY={y} />)
-
-        setLocation(newLocation);
-    }
-
-    // const handleRobot = (event) => {
-    //     console.log("Robot is clicked!");
-    // }
+    let width = useWindowWidth();
 
     return ( 
         <MainWrapper>
-            {/* <ImageStyled 
-                src={extendedCity} 
-                alt="robot city img" 
-                // onClick={(event) => handleClick(event)}
-                >
-            </ImageStyled> */}
-            {/* {location} */}
-            <Mapper />
+            <Mapper
+                responsive={true}
+                parentWidth={width}
+            />
         </MainWrapper>
      );
 }
@@ -55,17 +23,5 @@ const MainWrapper = styled.main`
     left: 0;
 `;
 
-const ImageStyled = styled.img`
-    max-width: 100%;
-`;
-
-// const RobotStyled = styled.div`
-//     position: absolute;
-//     top: 143px;
-//     left: 129px;
-//     width: 143px;
-//     height: 264px;
-//     background-color: rgba(0,255,0,0.3);;
-// `;
  
 export default Main;
--- src/components/mapper.js
+++ src/components/mapper.js
@@ -1,21 +1,33 @@
 import ImageMapper from 'react-img-mapper';
-import extendedCity from "../assets/images/extended_city.jpg";
 import areas from "../assets/JSON/areas.json"
 
 const onClick = (e) => {
-    console.log(e);
+    switch (e.title) {
+        case "Robot":
+            console.log("You found the Robot!");
+            break;
+        default:
+            console.log("Keep Looking!");
+    }
 }
 
-
 const Mapper = props => {
-    const URL =  "https://raw.githubusercontent.com/naknamu/wheres-waldo/main/src/assets/images/robot_city.jpg";
+    const URL =  "https://raw.githubusercontent.com/naknamu/wheres-waldo/main/src/assets/images/extended_city.jpg";
     const MAP = {
       name: 'my-map',
-      // GET JSON FROM BELOW URL AS AN EXAMPLE
       areas: areas,
     };
     
-    return <ImageMapper src={URL} map={MAP} onClick={e => onClick(e)} />
+    return (
+        <ImageMapper 
+            src={URL} 
+            map={MAP} 
+            onClick={e => onClick(e)} 
+            stayHighlighted={true} 
+            responsive={props.responsive}
+            parentWidth={props.parentWidth}
+        />
+    )
 }
 
 export default Mapper;
