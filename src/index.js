7c5f86781639ac311ecbd10353687e3b04b4719e
naknamu <jodeldelvalle@gmail.com>
2023-01-19 14:41:06 +0800

    Removed unnecessary file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import GlobalStyle from "./theme/GlobalStyle";
 import { ThemeProvider } from "styled-components";
